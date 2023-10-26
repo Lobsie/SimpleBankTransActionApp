5d3d6ad897e6bcbf1fc16919d2b8ae720713d3a3
Lobsie <gijs.duyck@gmail.com>
2023-10-26 23:51:13 +0200

    Add unversioned files

---
+++ SimpleBankTansactionApp.Api.Infrastructure/Migrations/20231026215025_InitialMigration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleBankTansactionApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OutgoingBankAccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IncomingBankAccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankTransactions_BankAccounts_IncomingBankAccountId",
+                        column: x => x.IncomingBankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BankTransactions_BankAccounts_OutgoingBankAccountId",
+                        column: x => x.OutgoingBankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_ClientId",
+                table: "BankAccounts",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransactions_IncomingBankAccountId",
+                table: "BankTransactions",
+                column: "IncomingBankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransactions_OutgoingBankAccountId",
+                table: "BankTransactions",
+                column: "OutgoingBankAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
