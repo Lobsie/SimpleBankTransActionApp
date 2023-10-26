95a875c02964bda68f3f03f09812c586ae7f536f
Lobsie <gijs.duyck@gmail.com>
2023-10-26 23:18:19 +0200

    Add entities

--- SimpleBankTransactionApp.Api.Core/Entities/BankAccounts/BankAccount.cs
+++ SimpleBankTransactionApp.Api.Core/Entities/BankAccounts/BankAccount.cs
@@ -1,9 +1,15 @@
-﻿using SimpleBankTransactionApp.Core.Entities.Base;
+﻿using SimpleBankTransactionApp.Core.Entities.BankTransactions;
+using SimpleBankTransactionApp.Core.Entities.Base;
+using SimpleBankTransactionApp.Core.Entities.Clients;
 
 namespace SimpleBankTransactionApp.Core.Entities.BankAccounts
 {
     public class BankAccount : EntityBase
     {
-        
+        public decimal Balance { get; set; }
+        public Guid ClientId { get; set; }
+        public Client Client { get; set; }
+        public ICollection<BankTransaction> OutgoingBankTransactions { get; set; }
+        public ICollection<BankTransaction> IncomingBankTransactions { get; set; }
     }
 }
---
+++ SimpleBankTransactionApp.Api.Core/Entities/BankTransactions/BankTransaction.cs
@@ -0,0 +1,14 @@
+﻿using SimpleBankTransactionApp.Core.Entities.BankAccounts;
+using SimpleBankTransactionApp.Core.Entities.Base;
+
+namespace SimpleBankTransactionApp.Core.Entities.BankTransactions
+{
+    public class BankTransaction : EntityBase
+    {
+        public Guid OutgoingBankAccountId { get; set; }
+        public BankAccount OutgoingBankAccount  { get; set; }
+        public Guid IncomingBankAccountId { get; set; }
+        public BankAccount IncomingBankAccount { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
