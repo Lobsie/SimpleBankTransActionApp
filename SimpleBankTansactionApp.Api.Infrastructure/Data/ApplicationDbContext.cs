6193132c3d82cf32b75211aa0a7936cd70c282bc
Lobsie <gijs.duyck@gmail.com>
2023-10-26 23:26:21 +0200

    Add applicationdbcontext

---
+++ SimpleBankTansactionApp.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBankTransactionApp.Core.Entities.BankAccounts;
+using SimpleBankTransactionApp.Core.Entities.BankTransactions;
+using SimpleBankTransactionApp.Core.Entities.Clients;
+
+namespace SimpleBankTansactionApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<BankTransaction> BankTransactions { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+    }
+}
