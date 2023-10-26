5a0f3d4c5bc13465b93224ff15c3cdcbc6b18ad9
Lobsie <gijs.duyck@gmail.com>
2023-10-27 00:21:53 +0200

    Create repository interface and implement

---
+++ SimpleBankTansactionApp.Api.Infrastructure/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBankTansactionApp.Infrastructure.Data;
+using SimpleBankTransactionApp.Core.Entities.Base;
+using SimpleBankTransactionApp.Core.Interfaces.Repositories;
+
+namespace SimpleBankTansactionApp.Infrastructure.Repositories.Base
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : EntityBase
+    {
+        protected readonly ApplicationDbContext _dbContext;
+
+        public BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid Id)
+        {
+            var entity = await _dbContext.Set<TEntity>().FindAsync(Id);
+
+            return entity;
+        }
+
+        public IQueryable<TEntity> GetAllAsync()
+        {
+            var entities = _dbContext.Set<TEntity>().AsNoTracking();
+
+            return entities;
+        }
+
+        public async Task<IEnumerable<TEntity>> ListAllAsync()
+        {
+            var entities = await GetAllAsync().ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+            
+            return entity;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Deleted;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ SimpleBankTransactionApp.Api.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBankTransactionApp.Core.Entities.Base;
+
+namespace SimpleBankTransactionApp.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity>
+        where TEntity : EntityBase
+    {
+        Task<TEntity> GetByIdAsync(Guid Id);
+        IQueryable<TEntity> GetAllAsync();
+        Task<IEnumerable<TEntity>> ListAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
